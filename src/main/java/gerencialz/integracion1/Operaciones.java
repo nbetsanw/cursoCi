0d520a24ffce10729e08c680c5975475065b888a
Néstor Bethencourt <nbetsanw@gobiernodecanarias.org>
2021-05-24 10:17:23 +0100

    se añade la operacion suma

--- src/main/java/gerencialz/integracion1/Operaciones.java
+++ src/main/java/gerencialz/integracion1/Operaciones.java
@@ -2,6 +2,10 @@ package gerencialz.integracion1;
 
 public class Operaciones {
 
+	public static int suma(int a, int b) {
+		return a+b;
+	}
+	
 	public static int resta(int a, int b) {
 		return a-b;
 	}
